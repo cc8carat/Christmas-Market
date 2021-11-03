d96b5cb5292fdb5f7334933da1c5afdf37ad2a63
Anna Zdyb <ania@annas-imac.localdomain>
2021-11-03 17:11:15 +0100

    fixing a coupple of bugs

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,12 @@ import React from 'react';
 const About = () => {
 
     return(
-        <h2>🎄 This is some info about us the CREATORS :) </h2>
+        <div>
+         <h2>🎄 This is some info about us the CREATORS :) </h2>
+         <h2 className="xmasTree">🎄 </h2>
+         <h2 className="xmasTree">🎁 </h2>
+         <h2 className="xmasTree">🦌 </h2>
+        </div>
     )
 }
 
