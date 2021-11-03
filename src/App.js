50b48747c501392355d8216dd25d10efc90589e9
Anna Zdyb <ania@annas-imac.localdomain>
2021-11-03 16:23:33 +0100

    adding navigation component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 
 import './App.css';
 import Picture from './components/Picture'
+import Navibar from './components/Navibar'
+import { Switch, link, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     <h1>This is s journey through the europes Christmas markets</h1>
-     <div className="picture">
-       <Picture />
-     </div>
+    <div className="App appMainDiv">
+      <div className="headerDiv">
+         <h1>This is a journey through the Europe's Christmas markets</h1>
+       </div>
+       <div className="navigation">  
+                  <Navibar /> 
+       </div>
+       <div className="picture">
+            <Picture />
+        </div>
     </div>
   );
 }
--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,19 +1,31 @@
 import React, {useState, useEffect} from 'react';
+//importing images
+import XmasTree from '../pictures/XmasTree.jpg'
+import XmasTree2 from '../pictures/XmasTree2.jpg'
+
 
 const Picture = () => {
 
-    return (
-    <div className="main-div"> 
-      <div className="backSide">
-        <img src="./pictures/XmasTree.jpg" alt="picture of a christmas tree" />
-       
-      </div>
-      <div className="frontSide">
-       <img src="./pictures/XmasTree2" alt="picture of a christmas tree" />
-      </div>
-    </div>
+  const [isActive, setActive] = useState(true);
 
-    );
+  const toggleClass = () => {
+    setActive(!isActive);
+  };
+
+    return (
+  <section>
+    <div 
+    className={isActive ? 'toggleCard': 'main-div'}
+    onClick={toggleClass}> 
+       <div className="frontSide">
+         <img src={XmasTree} alt="picture 1" />;
+       </div>
+       <div className="backSide">
+         <img src={XmasTree2} alt="picture of a christmas tree" />
+       </div>
+    </div>
+    </section>
+    )
 }
 
 export default Picture;
