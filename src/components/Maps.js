e1af70122b4bd239c3024e37eca16535e02d66a9
ZhiJuanTang <tangzhijuan56@gmail.com>
2021-11-11 01:45:41 +0100

    add className to maps to make it smaller

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -4,9 +4,11 @@ import { Map, Marker } from "pigeon-maps";
 const Maps = ({ lat, lon }) => {
   return (
     <>
+      <div className="marketListContainer">
       <Map height={300} defaultCenter={[lat, lon]} defaultZoom={11}>
         <Marker width={50} anchor={[lat, lon]} />
       </Map>
+      </div>
     </>
   );
 };
