7a7e81665442e6f2d192ffb4b6095e5e15c3960a
anna.z.z.a.g <89396456+anna-1980@users.noreply.github.com>
2021-11-30 19:51:15 +0100

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,6 +7,24 @@ const Contact = () => {
           <h2>✉️</h2>
         </button>
       </div>
+         <div className="form">
+        <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true" >
+          <div className="nameInput">
+            <label htmlFor="name">Name</label> 
+            <input type="text" id="name" name="name" placeholder="Enter your name here" />
+          </div>
+          <div className="emailAddress">
+            <label htmlFor="email" >Email</label>  
+            <input type="email" name="email" id="email" placeholder="Enter your email here" />
+          </div>
+          <div className="contactMessage">
+            <label htmlFor="message" className="msgText" >Message</label><br />
+            <textarea  name="message" id="message" cols="50" rows="10"></textarea>
+          </div>
+          <div data-netlify-recaptcha="true"></div>
+          <div className="buttonDiv">
+          <button type="submit" className="formButton">Send</button>
+          </div>
       <div className="bouncer">
         <div>Anna</div>
         <div></div>
